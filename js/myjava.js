277ea7bce9bc7319d2b7891c1c68a1f1004ab88c
joudi awameh <awamehj@gmail.com>
2020-09-07 15:56:49 +0300

    my new code

---
+++ js/myjava.js
@@ -0,0 +1,91 @@
+'use strict';
+
+
+var username = prompt('what is your name ???');
+    alert('welcome '+username+ ' nice to meet you');
+
+
+
+    var button = document.getElementById("hide");
+    function hide(){
+
+
+var myname = prompt('is my name joudi ?').toLowerCase();
+if(myname == 'yes' ||myname == 'y'){
+    // consol.log( right my name is joudi);
+    alert('right my name is joudi :)');
+} else if(myname == 'no' || myname == 'n'){
+    // consol.log(sorry your answered wrong but  it is not problem );
+    alert('sorry your answered wrong but  it is not problem');
+}else {
+    alert('try maybe you can know the answer')
+}
+
+
+
+var myage = prompt('Am i 20 years old ???').toLowerCase();
+
+if(myage == 'yes' || myage == 'y'){
+    // consol.log(wrong i am not 20 ,but it\'s okay maybe you will know the next quastion);
+    alert('wrong i am not 20 ,but it\'s okay maybe you will know the next quastion');
+    
+} else if(myage == 'no' || myage == 'n'){
+    // consol.log(right i am not 20 i am 19 );
+    alert('right i am not 20 i am 19 ;)');
+}else {
+    alert('try maybe you can know the answer')
+}
+
+
+
+
+var myeducation = prompt('Did I graduate from the university? ').toLowerCase();
+if(myeducation == 'yes' || myeducation == 'y'){
+    // consol.log(You were very close, but no problem, you can try the next question );
+    alert('You were very close, but no problem, you can try the next question');
+} else if(myeducation  == 'no' || myname == 'n'){
+    // consol.log(wow You have answered correctly );
+    alert('wow You have answered correctly ');
+}else {
+    alert('try maybe you can know the answer')
+}
+
+
+ 
+var myunivercity = prompt('Do i want to study computer science???').toLowerCase();
+if(myunivercity == 'yes' ||myunivercity == 'y'){
+    // consol.log(omg you know me well );
+    alert('omg you know me well');
+} else if(myunivercity == 'no' || myunivercity == 'n'){
+    // consol.log(wrong sorry i like computer science );
+    alert('wrong sorry i like computer science ;)');
+}else {
+    alert('try maybe you can know the answer')
+} 
+
+
+
+var mygols = prompt('Do you think i want to be professional in code ???? ').toLowerCase();
+if(mygols == 'yes' ||mygols == 'y'){
+    // consol.log(perfect that\'s it );
+    alert('perfect that\'s it ');
+} else if(mygols  == 'no' || mygols == 'n'){
+    // consol.log(wrong i want to be professional in code );
+    alert('wrong i want to be professional in code  ');
+}else {
+    alert('try maybe you can know the answer')
+}
+
+
+alert('thank you  ' +username+ ' for giving me part of your time to get to know me and I hope you have fun ');
+    }
+
+
+
+
+
+
+ 
+
+
+
